c84090d1a53f34590a22680050b6d20e2cf8e51e
madebykings <admin@madebykings.com>
2025-11-19 10:31:48 +0000

    Update motors.py

--- motors.py
+++ motors.py
@@ -1,1 +1,20 @@
+import time
+import RPi.GPIO as GPIO
 
+
+def setup_motors(pin_map):
+    """
+    pin_map: dict like {"1": 22, "2": 23, "3": 24}
+    """
+    for pin in pin_map.values():
+        GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+
+
+def spin(pin, seconds):
+    """
+    Turn relay/motor ON for given time, then OFF.
+    Assumes HIGH = ON for your relay board.
+    """
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(seconds)
+    GPIO.output(pin, GPIO.LOW)
